1eb7b92a767c146b4d696ac90b72edaeed789728
MylesAndersson140 <134179125+MylesAndersson140@users.noreply.github.com>
2024-11-04 02:43:55 -0800

    L16 Dialog Commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+import "./style.css";
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+app.innerHTML = `
+    <button id="alertButton">Click me!</button>
+`;
+
+const button = document.querySelector<HTMLButtonElement>("#alertButton")!;
+button.addEventListener("click", (): void => {
+  alert("You clicked the button!");
+});
